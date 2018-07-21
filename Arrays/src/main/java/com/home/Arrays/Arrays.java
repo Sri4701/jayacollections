55e0ee43cee6855529f37fa9a89515375af42bf5
jayar <jayar@DESKTOP-CRNPDN1>
2018-07-21 11:13:12 -0400

    first project

---
+++ Arrays/src/main/java/com/home/Arrays/Arrays.java
@@ -0,0 +1,13 @@
+package com.home.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class Arrays 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
