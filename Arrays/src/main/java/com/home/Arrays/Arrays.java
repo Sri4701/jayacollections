16ab7be76aa5704a4c5956d50736488d75554709
jaya <jaya@192.168.1.9>
2018-07-21 11:19:58 -0400

    Added new line task#1345

--- Arrays/src/main/java/com/home/Arrays/Arrays.java
+++ Arrays/src/main/java/com/home/Arrays/Arrays.java
@@ -3,11 +3,13 @@ package com.home.Arrays;
 /**
  * Hello world!
  *
+ *sp added new line
  */
 public class Arrays 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Add new line sp" );
     }
 }
